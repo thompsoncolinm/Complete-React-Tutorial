5cde33c2fcbc86c91f58549f3bf0aa53448cae62
Colin Thompson <thompsoncolinm@gmail.com>
2024-07-16 10:29:25 -0400

    leawrning wip

---
+++ mayan-mayhem/app/components/BlogList.js
@@ -0,0 +1,15 @@
+const BlogList = ({blogs, title}) => {
+    return ( 
+        <div className="blog-list">
+        <h2>{ title }</h2>
+        {blogs.map(blog => (
+          <div className="blog-preview" key={blog.id} >
+            <h3>{ blog.title }</h3>
+            <p>Written by { blog.author }</p>
+          </div>
+        ))}
+        </div>
+     );
+}
+ 
+export default BlogList;
--- mayan-mayhem/app/pages/home.js
+++ mayan-mayhem/app/pages/home.js
@@ -1,19 +1,19 @@
 'use client';
 import { useState } from 'react';
+import BlogList from '../components/BlogList';
 
 const Home = () => {
-    const [name, setName] = useState('mario');
-    const [age, setAge] = useState(25);
-    const handleClick = () => {
-        setName('luigi');
-        setAge(30);
-    }
-
-    return ( <div className="home">
-        <h2>Home Page with button</h2>
-        <p>{name} is {age}</p>
-        <button onClick={handleClick}>Click me</button>
-    </div> );
-}
- 
-export default Home;
+    const [blogs, setBlogs] = useState([
+      { title: 'My new website', body: 'lorem ipsum...', author: 'mario', id: 1 },
+      { title: 'Welcome party!', body: 'lorem ipsum...', author: 'yoshi', id: 2 },
+      { title: 'Web dev top tips', body: 'lorem ipsum...', author: 'mario', id: 3 }
+    ])
+  
+    return (
+      <div className="home">
+        <BlogList blogs={blogs} title="All Blogs!" />
+      </div>
+    );
+  }
+   
+  export default Home;
